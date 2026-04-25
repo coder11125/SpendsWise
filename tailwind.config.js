f7c36fcdfbe9a0dd955865d95e410ecd8bc63ec3
Shashvath <shashvathpuppala.21cmis0265@meruinternationalschool.com>
2026-04-25 19:58:00 +0530

    Make the 'Download CSV button' visible

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./auth.js",
+    "./script.js",
+    "./src/**/*.{html,js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
